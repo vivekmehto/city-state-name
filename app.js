2e41d35aa82c97be42559348e0d1fc1f4111aae2
vivekmehto <vivekmehto.webdev@gmail.com>
2021-12-08 21:56:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
 const url =
   "https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/states%2Bcities.json";
 
@@ -5,3 +8,6 @@ const cities = [];
 fetch(url)
   .then((res) => res.json())
   .then((data) => cities.push(...data));
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
